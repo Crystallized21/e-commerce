aeebcc87c3fa38b5431a71c3100f02f182acb052
Michael Bui <58403578+Crystallized21@users.noreply.github.com>
2024-03-22 22:02:54 +1300

    Delete src directory

--- src/pages/LoginSignup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LoginSignup(props) {
-    return (
-        <div></div>
-    );
-}
-
-export default LoginSignup;
--- src/pages/ShopCategory.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ShopCategory(props) {
-    return (
-        <div></div>
-    );
-}
-
-export default ShopCategory;
